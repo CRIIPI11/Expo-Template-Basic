684ef08691850b6877859cd36c1224f72ed1d5f7
Cristhian Molina <cdml2001@hotmail.com>
2024-10-12 22:10:32 -0400

    created circle view container

---
+++ components/core/shapeView/CircleView.tsx
@@ -0,0 +1,66 @@
+import { View, ViewProps } from "react-native";
+
+type CircleViewProps = {
+  children: React.ReactNode;
+  size?: "sm" | "md" | "lg" | "xl" | "xxl";
+  position?: "left" | "center" | "right";
+} & ViewProps;
+
+const useSize = (size: CircleViewProps["size"]) => {
+  switch (size) {
+    case "sm":
+      return 50;
+    case "md":
+      return 100;
+    case "lg":
+      return 150;
+    case "xl":
+      return 200;
+    case "xxl":
+      return 250;
+    default:
+      return 100;
+  }
+};
+
+const usePosition = (position: CircleViewProps["position"]) => {
+  switch (position) {
+    case "left":
+      return "flex-start";
+    case "center":
+      return "center";
+    case "right":
+      return "flex-end";
+    default:
+      return "center";
+  }
+};
+
+const CircleView = ({
+  children,
+  size,
+  position,
+  ...props
+}: CircleViewProps) => {
+  return (
+    <View
+      style={{
+        borderWidth: 0.5,
+        width: useSize(size),
+        height: useSize(size),
+        borderRadius: useSize(size) / 2,
+        justifyContent: "center",
+        alignItems: usePosition(position),
+        alignSelf: "center",
+        overflow: "hidden",
+        paddingVertical: 8,
+        paddingHorizontal: 8,
+      }}
+      {...props}
+    >
+      {children}
+    </View>
+  );
+};
+
+export default CircleView;
